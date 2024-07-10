8cfe8120ffe9266ac44c3fb06f5b188b54980d1c
mattbrain <matt.brain@gmail.com>
2024-07-10 15:51:57 +0100

    modified jsonata-java service address

--- src/Exerciser.js
+++ src/Exerciser.js
@@ -450,7 +450,7 @@ class Exerciser extends React.Component {
 	    data.append('rule',this.state.jsonata);
         var text_result = null;
         try {
-          const response = await fetch('https://jsonata.appleby-analytics.com/hello', {
+          const response = await fetch('https://jsonata.appleby-analytics.com/parseJsonata', {
             body: data,
             method: 'POST'
           });
